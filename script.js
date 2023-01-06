cd149ffbe3337b123a330099bdd93e23d23e02ef
StjeanPhil <philippe_spellerin@hotmail.ca>
2023-01-05 21:08:08 -0500

    added 7th chord

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let currentScale='major'
-let currentKey='';
+let currentKey='a';
 let current,currentlyShowing;
 
 let pitch=['c','csharp','d','dsharp','e','f','fsharp','g','gsharp','a','asharp','b']
-
+ 
 const scales={
     "major":[2,2,1,2,2,2,1],
     'minor':[2,1,2,2,1,2,2]
@@ -28,6 +28,9 @@ let setKeys=(p)=>{
        currentKey=pitch[(idx-1)%12]
        document.getElementById("currentPitch").innerHTML=currentKey
     }
+    
+    document.getElementById("set").onclick = ()=>{showKeys(currentScale,currentKey)}
+    document.getElementById("reset").onclick=refreshKeys
 }
 
 //Selection from user
@@ -47,12 +50,54 @@ let selectScale=(scale)=>{
 
     curr=document.getElementsByClassName(scale)
     curr[0].classList.add("active")
-
-
 }
+// flash the notes from the chord with the right number from the roman numeral notations
+const showChord=(n)=>{
+    refreshChord()
+    showActiveChordBtn(n)
+    nb=n
+    let idx = pitch.indexOf(currentKey)
+    let max=idx+12
 
-const safesetKeys=()=>{
-    showKeys(currentScale,currentKey)
+    for(var x=0;x<24;x++){
+        elems=document.getElementsByClassName(pitch[(x+idx)%12])
+        console.log(pitch[(x+idx)%12])
+        console.log(elems[0].classList.contains("inScale"))
+        console.log(nb)
+        if (elems[0].classList.contains("inScale")){
+            nb-=1
+            if (nb==0 || nb==-2 ||nb==-4){
+                elems[(x+idx>=12?1:0)].classList.add("inChord")
+            }
+        }
+    }  
+}
+const showActiveChordBtn=(n)=>{
+    var elems=document.getElementsByClassName("activeChord")
+    if (elems.length>0 ){elems[0].classList.remove("activeChord")}
+    switch(n){
+        case 1:
+            document.getElementById("i").classList.add("activeChord")
+            break
+        case 2:
+            document.getElementById("ii").classList.add("activeChord")
+            break
+        case 3:
+            document.getElementById("iii").classList.add("activeChord")
+            break
+        case 4:
+            document.getElementById("iv").classList.add("activeChord")
+            break
+        case 5:
+            document.getElementById("v").classList.add("activeChord")
+            break
+        case 6:
+            document.getElementById("vi").classList.add("activeChord")
+            break
+        case 7:
+            document.getElementById("vii").classList.add("activeChord")
+            break
+    }
 }
 const showKeys=(s,key)=>{
     refreshKeys()
@@ -62,7 +107,6 @@ const showKeys=(s,key)=>{
 
     for(var i=0;i<7;i++){
         idx+=scale[i]
-        console.log(idx)
         var toChange= document.getElementsByClassName(pitch[idx%12])
         for (var j=0;j<toChange.length;j++){
             toChange[j].classList.add("inScale")
@@ -77,6 +121,17 @@ const refreshKeys=()=>{
             k[i].classList.remove("inScale")
         }
     }
+    refreshChord()
+}
+const refreshChord=()=>{
+    var k=document.getElementsByClassName("key")
+    for (var i =0; i<k.length;i++){
+        if (k[i].classList.contains("inChord")){
+            k[i].classList.remove("inChord")
+        }
+    }
+    var elems=document.getElementsByClassName("activeChord")
+    if (elems.length>0 ){elems[0].classList.remove("activeChord")}
 }
 //Start the page
 const init=()=>{
